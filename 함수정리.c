069b3163c2274a53b0f76898f52648f7bc0e7f82
Jaeeunykim <jaeeun.kje@gmail.com>
2017-06-06 11:56:40 +0900

    add all the code in whitebook

---
+++ 함수정리.c
@@ -0,0 +1,87 @@
+함수정리
+chapter1.
+1. void copy(char to[], char from[])
+3. int getline(char line[], int maxline)
+3. int power(int m, int n)
+
+chapter2.
+1.strlen(char s[])
+2.atoi(char s[])
+3. lower(int c)
+4.rand(void)
+5.srand(unsigned int seed)
+6.squeeze(char s[], int c)
+7.stract(char s[], char t[])
+8.bitcount(unsigned x)
+
+chapter3.
+1.binsearch(int x, int v[], int n)
+2.atoi(char s[])
+3.sellsort(int v[], int n)
+4.reverse(char s[])
+5.void itoa(int n, char s[])
+6.int trim (char s[])
+
+
+chapter4.
+1.int strindex(char source[], char searchfor[])
+2.double atof(char s[])
+3.int getop(char [])
+4.void push(double)
+5.double pop(void)
+6.void printd(int n)
+7.void qsort(int v[], int i, int j)
+8.void swap(int v[], int i, int j)
+
+chapter5.
+1.int getch(void)
+2.void ungetch(int *pn)
+3.static char allocbuf[ALLOCSIZE]
+4.static char *allocp = allocbuf
+5.char* alloc (int n)
+6.void strcpy (char*s, char* t)
+7.int strcmp(char* s, char* t)
+8.int readlines(char* lineptr[], int nlines)
+9.void writeline(char* lineptr[], int nlines)
+10.void qsort(char( lineptr[], int left, int right)
+11.char* month_name(int n)
+12.int numcmp(char*, char*)
+13.void cdl(void)
+14.void dirdcl(void)
+15.int gettoken(void)
+
+
+chapter6.
+struct point makepoint(int x, int y)
+struct addpoint(struct point p1, struct point p2)
+int ptinrect(struct point p, struct rect r)
+struct rect canonrect(struct rect r)
+int getword(char* word, int lim)
+struct treenode* addtree(struct tnode *p, char* w)
+void treeprint(struct tnode *p)
+struct tnode* talloc(void)
+char* strdup (char*s)
+unsigned hash(char*s)
+struct nlist* lookup(char*s)
+struct nlist *install(char* name, char *defn)
+
+chapter7
+
+void minprintf(char* fmt,..)
+void filecopy(FILE *ifp, FILE* ofp)
+char* fgets(char*s, int n, FILE*iop)
+char* fputs(char*s, FILE*iop)
+
+chapter8	
+void error(char* fmt,..)
+int get(int fd, long pos, char* buf, int n)
+FILE *fopen(char* name, char* mode)
+int_fillbuf(FILE *fp)
+void fsize(char* name)
+void dirwalk(char* dir, void (*fcn)(char*))
+DIR *opendir(char* dirname)
+void closedir(DIR *dp)
+Dirent *readdir(DIR *dp)
+static Header * morecore(unsigned nu)
+void free(void *ap)
+
